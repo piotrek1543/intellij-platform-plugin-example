a9eb23b0c5d0fdc59597a51b476df252ce431df4
GitHub Action <action@github.com>
2024-10-01 15:29:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/piotrek1543/intellijplatformpluginexample/MyBundle.kt
+++ src/main/kotlin/com/github/piotrek1543/intellijplatformpluginexample/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.piotrek1543.intellijplatformpluginexample
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/piotrek1543/intellijplatformpluginexample/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/piotrek1543/intellijplatformpluginexample/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.piotrek1543.intellijplatformpluginexample.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.piotrek1543.intellijplatformpluginexample.MyBundle
+import com.github.piotrek1543.intellijplatformpluginexample.services.MyProjectService
 import javax.swing.JButton
 
 
